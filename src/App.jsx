43338b30028945fd53c74a2559641d0b39f295be
yousef <yousefshwaly@gmail.com>
2026-01-09 20:55:49 +0200

    Change background of Website

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Header from "./components/Header";
 export default function App() {
   return (
     <main>
+      <div className="pattern"/>
       <div className="wrapper">
         <Header />
       </div>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@
 export default function Header() {
   return (
     <header>
-      <img src="hero.png" alt="Hero Banner" />
+      <img src="/hero.png" alt="Hero Banner" />
       <h1>Find <span className="text-gradient">Movies</span> You'll Enjoy Without the Hassle</h1>
     </header>
   )
