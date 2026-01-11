94a8cc559e9c69e893484fc85ed7a0eefb65c1a0
yousef <yousefshwaly@gmail.com>
2026-01-11 14:08:49 +0200

    Make Search Optimization using debounce

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,15 @@ import Header from "./components/Header";
 import Search from "./components/Search";
 import useMovies from "./hooks/useMovies";
 import Movies from "./components/Movies";
+import { useDebounce } from "react-use";
 
 export default function App() {
   const [searchTerm, setSearchTerm] = useState("")
-  const {data , isLoading , error} = useMovies({query: searchTerm})
+  const [debouncedSearchTerm, setDebouncedSearchTerm] = useState("")
+
+  useDebounce(()=>{setDebouncedSearchTerm(searchTerm)}, 500, [searchTerm])
+
+  const {data , isLoading , error} = useMovies({query: debouncedSearchTerm})
   console.log(data , error , isLoading);
 
   return (
