f91a06faad08c84612359d42949624322e9c8444
yousef <yousefshwaly@gmail.com>
2026-01-11 17:25:23 +0200

    handle in appwrite the watching and searching count

---
+++ src/features/appwrite.js
@@ -0,0 +1,49 @@
+import { Client, ID, Query, TablesDB } from "appwrite";
+
+const PROJECT_ID = import.meta.env.VITE_APPWRITE_PROJECT_ID;
+const DATABASE_ID = import.meta.env.VITE_APPWRITE_DATABASE_ID;
+const TABLE_ID = import.meta.env.VITE_APPWRITE_COLLECTION_ID;
+const ENDPOINT = import.meta.env.VITE_APPWRITE_ENDPOINT;
+
+const client = new Client().setEndpoint(ENDPOINT).setProject(PROJECT_ID);
+
+const tablesDB = new TablesDB(client);
+export const updateSearchCount = async (searchTerm, movie) => {
+  // 1. Use Appwrite SDK to check if the search term exists in the database
+  try {
+    const result = await tablesDB.listRows({
+      databaseId: DATABASE_ID,
+      tableId: TABLE_ID,
+      queries: [Query.equal("searchTerm", searchTerm)],
+    });
+
+    // 2. If it does, update the count
+    if (result.rows.length > 0) {
+      const row = result.rows[0];
+
+      await tablesDB.updateRow({
+        databaseId: DATABASE_ID,
+        tableId: TABLE_ID,
+        rowId: row.id,
+        data: {
+          count: row.count + 1,
+        },
+      });
+      // 3. If it doesn't, create a new document with the search term and count as 1
+    } else {
+      await tablesDB.createRow({
+        databaseId: DATABASE_ID,
+        tableId: TABLE_ID,
+        rowId: ID.unique(),
+        data: {
+          searchTerm,
+          count: 1,
+          movie_id: movie.id,
+          poster_url: `https://image.tmdb.org/t/p/w500${movie.poster_path}`,
+        },
+      });
+    }
+  } catch (error) {
+    console.error(error);
+  }
+};
