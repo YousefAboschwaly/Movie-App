02cdcf99af87809e637f3d36a15ca07a05508751
yousef <yousefshwaly@gmail.com>
2026-01-12 18:52:53 +0200

    Refactor main and transfer config of TanstackQuery to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 export default function App() {
+  const queryClient = new QueryClient();
+
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home/>} />
-      </Routes>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 }
