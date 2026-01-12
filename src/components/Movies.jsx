7a976aa7010b2015112f9ac0948a9a1e56c572e3
yousef <yousefshwaly@gmail.com>
2026-01-12 18:38:42 +0200

    Add css of movie-details

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -1,25 +1,46 @@
 import { useState } from "react";
-import MovieCard from "./MovieCard";
-import Spinner from "./Spinner";
 import { useDebounce } from "react-use";
 import useMovies from "../hooks/useMovies";
+import MovieCard from "./MovieCard";
+import Spinner from "./Spinner";
 
-export default function Movies({searchTerm}) {
+export default function Movies({ searchTerm }) {
+  const [debouncedSearchTerm, setDebouncedSearchTerm] = useState("");
+  useDebounce(
+    () => {
+      setDebouncedSearchTerm(searchTerm);
+    },
+    1000,
+    [searchTerm]
+  );
 
-  const [debouncedSearchTerm, setDebouncedSearchTerm] = useState("")
-  useDebounce(()=>{setDebouncedSearchTerm(searchTerm)}, 1000, [searchTerm])
+  const {
+    data: movies,
+    isLoading,
+    error,
+  } = useMovies({ query: debouncedSearchTerm });
 
-  const {data:movies , isLoading , error} = useMovies({query: debouncedSearchTerm})
+  if (isLoading)
+    return (
+      <div className="flex justify-center items-center">
+        <Spinner />
+      </div>
+    );
+  if (error)
+    return (
+      <p className="text-red-500">
+        {error?.message || error?.toString() || "An error occurred"}
+      </p>
+    );
 
-    if (isLoading) return <div className="flex justify-center items-center"><Spinner /></div>;
-  if (error) return <p className="text-red-500">{error?.message || error?.toString() || "An error occurred"}</p>;
-
-  return <section className="all-movies ">
-    <h2 className="mt-40">All Movies</h2>
-    <ul>
-      {movies.map((movie) => (
-       <MovieCard movie={movie} key={movie.id} />
-      ))}
-    </ul>
-  </section>;
+  return (
+    <section className="all-movies ">
+      <h2 className="mt-40">All Movies</h2>
+      <ul>
+        {movies.map((movie) => (
+          <MovieCard movie={movie} key={movie.id} />
+        ))}
+      </ul>
+    </section>
+  );
 }
