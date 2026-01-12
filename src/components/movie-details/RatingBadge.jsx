ad9e9b4729df1fdf5423029f1418192d249cc57e
yousef <yousefshwaly@gmail.com>
2026-01-13 00:37:47 +0200

    handle the Ui of header and RatingBadge

--- src/components/movie-details/RatingBadge.jsx
+++ src/components/movie-details/RatingBadge.jsx
@@ -2,18 +2,18 @@ import { Star, TrendingUp } from "lucide-react";
 
 export const RatingBadge = ({ rating, votes, trending }) => {
   return (
-    <div className="flex items-center gap-2 md:gap-3 flex-wrap">
-      <div className="flex items-center gap-2 px-4 py-2 rounded-lg font-semibold bg-[hsl(252,32%,18%)] border border-[hsl(252,32%,25%)] text-xs md:text-sm">
-        <Star className="w-3 h-3 md:w-4 md:h-4 fill-yellow-400 text-yellow-400" />
+    <div className="flex items-center gap-2.5 md:gap-3 flex-wrap text-light-200">
+      <div className="flex items-center gap-2.5 px-4 py-2 rounded-md font-semibold bg-secondary text-xs md:text-[16px] ">
+        <Star className="w-3 h-3 md:w-5 md:h-5 fill-yellow-400 text-yellow-400" />
         <span>
-          <span className="text-[hsl(240,20%,91%)] font-semibold">{rating}</span>
-          <span className="text-[hsl(240,30%,75%)]">/10 ({votes})</span>
+          <span className="text-white font-semibold">{rating}</span>
+          <span>/10 ({votes})</span>
         </span>
       </div>
       {trending && (
-        <div className="flex items-center gap-1.5 px-3 py-2 rounded-lg text-sm font-medium bg-[hsl(252,32%,18%)] border border-[hsl(252,32%,25%)]  md:text-sm">
-          <TrendingUp className="w-3 h-3 md:w-4 md:h-4 text-[hsl(256,68%,75%)]" />
-          <span className="text-[hsl(240,20%,91%)]">{trending}</span>
+        <div className="flex items-center gap-1.5 px-4 py-2 rounded-md text-sm font-semibold bg-secondary  md:text-[16px]">
+          <TrendingUp className="w-3 h-3 md:w-4 md:h-4 " />
+          <span >{trending}</span>
         </div>
       )}
     </div>
